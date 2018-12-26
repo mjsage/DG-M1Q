41d42cbf3a0523795d9fbbfe7bfa65e828395d6d
mjsage <melsage@gmail.com>
2018-12-26 23:19:01 +0000

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -2,41 +2,44 @@
 
 #ip address of time server
 timeserver=192.168.1.1
+home="/mnt/disc1/npc"
 
-#comment out to stop httpd from running.
-httpd="/mnt/disc1/busybox httpd"
+
+busybox="/mnt/disc1/busybox"
+httpd="$busybox httpd"
+ntpd="$busybox ntpd"
 
 #remount sd card as executable
+
 mount -t vfat -o rw,exec,umask=000,remount /dev/mmcblk0p1 /mnt/disc1
 #
-echo " remounting SD with exec"
 
-#check if timeserver set and for busybox on sd card
+####mount vfat.img as /mnt/SD 
+####mount -t vfat -o rw,exec,umask=000 /mnt/disc1/m1q.vfat.img /mnt/SD
 
-if [ ! -z "$timeserver" ] && [ -e /mnt/disc1/busybox ]; then
+if [ -e $busybox ]; then 
 
-# get time
+	# get time
+	if [ ! -z "$timeserver" ]; then
+		$ntpd -q -p $timeserver
+	fi
 
-/mnt/disc1/busybox ntpd -q -p $timeserver
-fi
+#check for httpd config file
 
+	if [ -e /mnt/disc1/httpd.conf ]; then
 
-if [ -e /mnt/disc1/httpd.conf ]; then
+		conf="-c /mnt/disc1/httpd.conf"
+	else
+		conf=""
+	fi
 
-  conf="-c /mnt/disc1/httpd.conf"
-  else
-  conf=""
-  
-fi
-
-#launch http server
+#launch http server home= /mnt/disc1/npc
 
 #/mnt/disc1/httpd -h /mnt/disc1/npc $conf </dev/null &>/dev/null &
 
-if [ ! -z "$httpd"  ] && [ -e /mnt/disc1/busybox ]; then
-$httpd -h /mnt/disc1/npc $conf </dev/null &>/dev/null &
+	if [ ! -z "$httpd"  ] && [ ! -z "$home" ]; then
+		$httpd -h $home $conf -r M1Q </dev/null &>/dev/null &
+	fi
+	
+#-e $busybox 
 fi
-
-#
-
-
