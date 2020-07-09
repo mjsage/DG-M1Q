2ff6a529465745cdc24833c4e5b15a51fae91341
mjsage <melsage@gmail.com>
2020-07-09 13:49:52 +0100

    Update VLC.bat

--- npc/VLC.bat
+++ npc/VLC.bat
@@ -1,3 +1,13 @@
 rem vlc.exe.lnk "%CD%\%1" --demux=h264
 
-"%ProgramFiles(x86)%\VideoLAN\VLC\vlc.exe" "%1" --demux=h264
+rem "%ProgramFiles(x86)%\VideoLAN\VLC\vlc.exe" "%1" --demux=h264
+
+rem check for 64 bit vlc
+
+if exist "%ProgramFiles%\VideoLAN\VLC\vlc.exe" (
+
+"%ProgramFiles%\VideoLAN\VLC\vlc.exe" %1% --demux=h264
+
+) else (
+		"%ProgramFiles(x86)%\VideoLAN\VLC\vlc.exe" %1% --demux=h264
+)
